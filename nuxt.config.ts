07d7bbe8243cefa3dbab63cee62f6b974fae3687
Junhsiun <xjuunn@gmail.com>
2025-12-17 11:20:30 +0800

    feat:安装必要库

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss() as any],
+  },
+  css: ["./app/assets/app.css"],
+
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode',
+    'pinia-plugin-persistedstate',
+    '@vueuse/nuxt',
+    '@pinia/nuxt'
+  ]
 })
